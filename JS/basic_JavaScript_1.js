83a296965e94b70302740260819ca01d3d1d45bf
Bobby Rosson <brdrummer@hotmail.com>
2019-12-17 14:17:01 -0800

    Basic Javascript Projects

---
+++ JS/basic_JavaScript_1.js
@@ -0,0 +1,19 @@
+document.write("\"Hello World!\", said the man.");
+
+var A = "This is a string";
+
+document.write(A);
+
+document.write("Hello" + "This is a concatenated string");
+
+var concatenate = "This is also" + " a concatenated string as a variable"
+document.write(concatenate);
+
+var Animals = "Amphibians", Crocodile="Amphibian1", Frog="Amphibian2";
+document.write(Crocodile);
+
+var red ="Roses are red.";
+var red = red.fontcolor("red");
+document.write(red);
+
+document.write(1,000+400);
